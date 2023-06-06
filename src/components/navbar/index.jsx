0243821e5005c89f6ec05e0f5682f7145a341954
Camilo arias <camillo47@gmail.com>
2023-06-05 21:38:42 -0500

    position navbar elements 👨‍🎨

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -17,6 +17,11 @@ export default function Navbar () {
               <button onClick={logout}>Cerrar sesión</button>
             </li>
         </ul>
+        <div className={styles['navigation__right-block']}>
+          <button>
+            <svg className={styles['sandwidch-btn']} data-v-648cf678="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill="currentColor" d="M25 22.5c.5522847 0 1 .4477153 1 1s-.4477153 1-1 1H7c-.55228475 0-1-.4477153-1-1s.44771525-1 1-1h18Zm0-7.75c.5522847 0 1 .4477153 1 1s-.4477153 1-1 1H7c-.55228475 0-1-.4477153-1-1s.44771525-1 1-1h18ZM25 7c.5522847 0 1 .44771525 1 1s-.4477153 1-1 1H7c-.55228475 0-1-.44771525-1-1s.44771525-1 1-1h18Z"></path></svg>
+          </button>
+        </div>
     </nav>
   )
 }
