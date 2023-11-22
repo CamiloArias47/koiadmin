b8f0bc59dcabc97ce813bc67afdfdfe9fbc73162
Camilo Arias <camillo47@gmail.com>
2023-11-22 18:08:19 -0500

    add styles to colors form 🎨👨🏼‍🎨

--- src/components/products/colors-form/index.tsx
+++ src/components/products/colors-form/index.tsx
@@ -4,11 +4,20 @@ export default function ColorsForm(){
     return(
         <div className={style['colors-warpper']}>
           <div className={style['colors-warpper__color']}>
-            <InputField id="colorName[]" name='colorName[]' type='text' titlename='Nombre' required/>
-            <InputField id="colorAmount[]" name='colorAmount[]' type='number' titlename='Cantidad' required/>
-            <InputField id="colors" name='colors' type='color' titlename='Colores' required/>
+            <InputField id="colorName" name='colorName[]' type='text' titlename='Nombre' required/>
+            <InputField id="colorAmount" name='colorAmount[]' type='number' titlename='Cantidad' required/>
+            <div className={style.colors__input}>
+              <label htmlFor='colors' className={style.colors__label}>Color</label>
+              <input id="colors"  name='colors[]' className={style.colors__inputcolor} type='color' placeholder='Colores' required/>
+              <span className={style.colors__inputerrors}></span>
+            </div>
           </div>
-          <button className={style['colors__add-btn']}>Agegar color</button>
+          <button 
+            className={style['colors__add-btn']}
+            onClick={(e)=>{e.preventDefault()}}
+          >
+            Agegar color
+          </button>
         </div>
     )
 }
