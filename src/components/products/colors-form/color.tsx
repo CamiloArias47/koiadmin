d3f55bdf4cf65f9cc980cf317ca0efad16dff9fa
Camilo arias <camillo47@gmail.com>
2023-12-26 09:47:39 -0500

    adjust delete buttom 🎨🖌️

--- src/components/products/colors-form/color.tsx
+++ src/components/products/colors-form/color.tsx
@@ -3,7 +3,10 @@ import style from './colorform.module.css'
 export default function Color(){
     return(
         <div className={style['colors-warpper__color']}>
-            <button className={style['colors__delete']}>X</button>
+            <div className={style['colors__wrap-delete']}>
+                <span></span>
+                <button className={style['colors__delete']}>X</button>
+            </div>
             <div className={style['colors__inputs']}>
                 <InputField id="colorName" name='colorName[]' type='text' titlename='Nombre' required/>
                 <InputField id="colorAmount" name='colorAmount[]' type='number' titlename='Cantidad' required/>
